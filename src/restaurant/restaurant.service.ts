2f61aab09d76962b7d42f356876d7e6aeb850f7a
Vibin Joby <vibin2joby@gmail.com>
2021-12-02 23:00:27 -0500

    update getrestaurants for average ratings

--- src/restaurant/restaurant.service.ts
+++ src/restaurant/restaurant.service.ts
@@ -1,11 +1,12 @@
 import { Injectable } from '@nestjs/common'
 import { InjectRepository } from '@nestjs/typeorm'
-import { DeleteResult, Repository, UpdateResult } from 'typeorm'
+import { DeleteResult, Repository } from 'typeorm'
 
 import { CreateRestaurantInput } from './dto/create-restaurant.input'
 import { UpdateRestaurantInput } from './dto/update-restaurant.input'
 import { Restaurant } from './entities/restaurant.entity'
 import { User } from '../user/entities/user.entity'
+import { Review } from '../review/entities/review.entity'
 
 @Injectable()
 export class RestaurantService {
@@ -22,20 +23,58 @@ export class RestaurantService {
     return this.restaurantRepository.save(newRestaurant)
   }
 
-  findAll(limit: number, offset: number): Promise<[Restaurant[], number]> {
-    return this.restaurantRepository.findAndCount({
-      take: limit,
-      skip: offset,
-      relations: ['reviews', 'reviews.user', 'user'],
-    })
+  /**
+   * 
+   * `select coalesce((avg(rw.ratings)),0) as averageRatings,
+       rt.id as id, rt.address as address,
+        rt.restaurantName as restaurantName 
+        from restaurant as rt left join review as rw 
+        on rw.restaurantId = rt.id
+        group by rt.id,rt.address,rt.restaurantName
+        `
+   */
+  async findAll(limit: number, offset: number): Promise<[Restaurant[], number]> {
+    const restaurants = await this.restaurantRepository
+      .createQueryBuilder()
+      .leftJoin(Review, 'review', 'review.restaurantId = restaurant.id')
+      .select('restaurant.restaurantName', 'restaurantName')
+      .addSelect('coalesce(AVG(ratings),0)', 'averageRatings')
+      .addSelect('restaurant.id', 'id')
+      .addSelect('restaurant.address', 'address')
+      .addSelect('restaurant.contactNumber', 'contactNumber')
+      .groupBy('restaurant.id,restaurant.address,restaurant.restaurantName,restaurant.contactNumber')
+      .offset(offset)
+      .limit(limit)
+      .getRawMany()
+
+    const count = await this.restaurantRepository.count()
+    return [restaurants, count]
   }
 
-  findOne(id: number): Promise<Restaurant> {
-    return this.restaurantRepository.findOne(id, { relations: ['reviews', 'reviews.user', 'user'] })
+  async findOne(id: number): Promise<Restaurant> {
+    const result = await this.restaurantRepository.findOne(id, { relations: ['reviews', 'reviews.user', 'user'] })
+    const averageRatings = await this.restaurantRepository
+      .createQueryBuilder()
+      .from(Review, 'review')
+      .select('coalesce(AVG(ratings),0)', 'averageRatings')
+      .getRawOne()
+
+    return { ...result, ...averageRatings }
   }
 
-  findRestaurantsByUserId(userId: number): Promise<Restaurant[]> {
-    return this.restaurantRepository.find({ where: { userId }, relations: ['reviews', 'reviews.user', 'user'] })
+  async findRestaurantsByUserId(userId: number): Promise<Restaurant[]> {
+    const restaurants = await this.restaurantRepository
+      .createQueryBuilder()
+      .leftJoin(Review, 'review', 'review.restaurantId = restaurant.id and restaurant.userId = :userId', { userId })
+      .select('restaurant.restaurantName', 'restaurantName')
+      .addSelect('coalesce(AVG(ratings),0)', 'averageRatings')
+      .addSelect('restaurant.id', 'id')
+      .addSelect('restaurant.address', 'address')
+      .addSelect('restaurant.contactNumber', 'contactNumber')
+      .groupBy('restaurant.id,restaurant.address,restaurant.restaurantName,restaurant.contactNumber')
+      .getRawMany()
+
+    return restaurants
   }
 
   async update(id: number, updateRestaurantInput: UpdateRestaurantInput): Promise<Partial<Restaurant>> {
